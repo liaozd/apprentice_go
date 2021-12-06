03b15985df9afb1782e2c2f5baef2295416a627a
liao zhuodi <liao_zd@hotmail.com>
2021-12-06 13:43:57 +0800

    update slice share address

---
+++ tmp_project/2021.12/21.12.06_slice.go
@@ -0,0 +1,43 @@
+package main
+
+//https://time.geekbang.org/column/article/330178
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func main() {
+	fun1()
+	fun2()
+}
+
+func fun1() {
+	fmt.Printf("Func1 子切片")
+	foo := make([]int, 5)
+	foo[3] = 42
+	foo[4] = 100
+	fmt.Printf("foo: %v \n", foo)
+	// 共享foo的内存地址
+	bar := foo[1:4]
+	bar[1] = 99
+	fmt.Printf("boo: %v \n", bar)
+	fmt.Printf("foo: %v \n", foo)
+}
+
+func fun2() {
+	fmt.Println("\nfun2 append a")
+	a := make([]int, 32)
+	b := a[1:16]
+	fmt.Printf("a address: %p, value: %d \n", a, a)
+	fmt.Printf("b address: %p, value: %d \n", b, b)
+
+	// 对 a 做了一个 append()的操作，这个操作会让 a 重新分配内存
+	a = append(a, 1)
+	fmt.Printf("a address after append: %p value: %d \n", a, a)
+}
+
+func fun3() {
+	path := []byte("A/BB")
+	sepIndex := bytes.IndexByte(path, '/')
+}
