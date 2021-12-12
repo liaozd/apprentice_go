7ed81213e69605d3e4e243752509c4407af30d62
Liao Zhuodi <liao_zd@hotmail.com>
2021-12-12 18:17:34 +0800

    go switch

---
+++ daily_pracice/2021.12/12.12_switch.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Unhex E %d\n", unhex('E'))
+
+	fmt.Printf("Should ')' escape? %b", shouldEscape(')'))
+}
+
+func unhex(c byte) byte {
+	switch {
+	case '0' <= c && c <= '9':
+		return c - '0'
+	case 'a' <= c && c <= 'f':
+		return c - 'a' + 10
+	case 'A' <= c && c <= 'F':
+		return c - 'A' + 10
+	}
+	return 0
+}
+
+func shouldEscape(c byte) bool {
+	switch c {
+	case ' ', '?', '&', '=', '#', '+', '%':
+		return true
+	}
+	return false
+}
