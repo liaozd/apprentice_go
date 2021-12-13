6eb8542939ce22bb6b7f75cd331021ef3c642a2c
liaozd <liao_zd@hotmail.com>
2021-12-13 11:02:44 +0800

    update panic

--- daily_pracice/2021.12/12.13_assertion.go
+++ daily_pracice/2021.12/12.13_assertion.go
@@ -9,7 +9,11 @@ func main() {
 	var x interface{} = 7
 	i := x.(int)
 	fmt.Println(reflect.TypeOf(i))
-	j := x.(int32)
-	fmt.Println(reflect.TypeOf(j))
+	value, ok := x.(int32)
+	if !ok {
+		fmt.Println("Type not ok")
+		return
+	}
+	fmt.Println(reflect.TypeOf(value))
 	// panic: interface conversion: interface {} is int, not int32
 }
