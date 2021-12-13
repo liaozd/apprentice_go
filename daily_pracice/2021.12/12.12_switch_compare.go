41df142a56867422cde0fa03563a90b07f6a9b24
liaozd <liao_zd@hotmail.com>
2021-12-13 09:24:28 +0800

    switch compare

---
+++ daily_pracice/2021.12/12.12_switch_compare.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Compare %d", Compare([]byte{'a', 'b'}, []byte{'a', 'b', 'c'}))
+}
+
+func Compare(a, b []byte) int {
+	for i := 0; i < len(a) && i < len(b); i++ {
+		switch {
+		case a[i] > b[i]:
+			return 1
+		case a[i] < b[i]:
+			return -1
+		}
+	}
+	switch {
+	case len(a) > len(b):
+		return 1
+	case len(a) < len(b):
+		return -1
+	}
+	return 0
+}
