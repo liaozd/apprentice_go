b0d65cae1bb89682846a1f39531209a3708527ee
liaozd <liao_zd@hotmail.com>
2021-12-14 15:44:39 +0800

    const iota

---
+++ daily_pracice/2021.12/12.14_const.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type ByteSize float64
+
+const (
+	_           = iota
+	KB ByteSize = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YB
+)
+
+func main() {
+	fmt.Println("KB:", KB)
+	fmt.Println("MB:", MB)
+	fmt.Println("GB:", GB)
+	fmt.Println("TB:", TB)
+	fmt.Println("PB:", PB)
+	fmt.Println("EB:", EB)
+	fmt.Println("ZB:", ZB)
+	fmt.Println("YB:", YB)
+}
