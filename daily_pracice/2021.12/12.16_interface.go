71e645fc58ac79ccda0c6b726ad38a80cca32909
Liao Zhuodi <liao_zd@hotmail.com>
2021-12-16 06:53:24 +0800

    interface

---
+++ daily_pracice/2021.12/12.16_interface.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Sequence []int
+
+func (s Sequence) Len() int {
+	return len(s)
+}
+
+func (s Sequence) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s Sequence) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s Sequence) String() string {
+	sort.Sort(s)
+	str := "["
+	for i, elem := range s {
+		if i > 0 {
+			str += " "
+		}
+		str += fmt.Sprint(elem)
+	}
+	return str + "]"
+}
+func main() {
+	s := Sequence{1, 2, 3}
+	ss := s.String()
+	fmt.Println(ss)
+}
