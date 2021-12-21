69713a1d94cecd5a23ce31142801381a8ed20da8
liao zhuodi <liao_zd@hotmail.com>
2021-12-21 17:05:03 +0800

    feat(greenlight): 配置content-type使用json

--- lets_go_further/greenlight/cmd/api/healthcheck.go
+++ lets_go_further/greenlight/cmd/api/healthcheck.go
@@ -6,7 +6,16 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// Create a fixed-format JSON response from a string. Notice how we're using a raw
+	// string literal (enclosed with backticks) so that we can include double-quote
+	// characters in the JSON without needing to escape them? We also use the %q verb to
+	// wrap the interpolated values in double-quotes.
+	js := `{"status": "available", "environment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	// At this point we know that encoding the data worked without any problems, so we
+	// can safely set any necessary HTTP headers for a successful response.
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write([]byte(js))
 }
