0b4006f1d8bf8591a74399c3878d7f8f573d5ded
Liao Zhuodi <liao_zd@hotmail.com>
2022-01-02 07:04:25 +0800

    feat(greenlight): 添加用户struct

---
+++ lets_go_further/greenlight/internal/data/users.go
@@ -0,0 +1,51 @@
+package data
+
+import (
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+	"time"
+)
+
+type User struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	Activated bool      `json:"activated"`
+}
+
+//Create a custom password type which is a struct containing the plaintext and hashed
+//versions of the password for a user. The plaintext field is a *pointer* to a string,
+//so that we're able to distinguish between a plaintext password not being present in
+//the struct at all, versus a plaintext password which is the empty string "".
+type password struct {
+	plaintext *string
+	hash      []byte
+}
+
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return err
+	}
+
+	p.plaintext = &plaintextPassword
+	p.hash = hash
+
+	return nil
+}
+
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
