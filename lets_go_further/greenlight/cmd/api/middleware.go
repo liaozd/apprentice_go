24a8481c64c82b8aecb33d27eaff2cac04f79390
Liao Zhuodi <liao_zd@hotmail.com>
2022-01-09 23:24:42 +0800

    feat(greenlight): 添加requireAuthenticateUser

--- lets_go_further/greenlight/cmd/api/middleware.go
+++ lets_go_further/greenlight/cmd/api/middleware.go
@@ -147,6 +147,19 @@ func (app *application) authenticate(next http.Handler) http.Handler {
 	})
 }
 
+func (app *application) requireAuthenticateUser(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := app.contextGetUser(r)
+
+		if user.IsAnonymous() {
+			app.authenticationRequiredResponse(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		user := app.contextGetUser(r)
