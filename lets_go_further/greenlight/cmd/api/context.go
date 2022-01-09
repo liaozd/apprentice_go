838ba27bc36fe04ba0bdd3d0819e5c1a88b15106
Liao Zhuodi <liao_zd@hotmail.com>
2022-01-09 22:43:07 +0800

    feat(greenlight): 添加要求用户激活中间件

--- lets_go_further/greenlight/cmd/api/context.go
+++ lets_go_further/greenlight/cmd/api/context.go
@@ -22,10 +22,11 @@ func (app *application) contextSetUser(r *http.Request, user *data.User) *http.R
 // time that we'll use this helper is when we logically expect there to be User struct
 // value in the context, and if it doesn't exist it will firmly be an 'unexpected' error.
 // As we discussed earlier in the book, it's OK to panic in those circumstances.
-func (app *application) contextGetUser(r *http.Request, user *data.User) *data.User {
+func (app *application) contextGetUser(r *http.Request) *data.User {
 	user, ok := r.Context().Value(userContextKey).(*data.User)
 	if !ok {
 		panic("missing user value in request context")
 	}
+
 	return user
 }
