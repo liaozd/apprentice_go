c67a25155cab3dcdbaa93b911ce749ea924820cf
Liao Zhuodi <liao_zd@hotmail.com>
2022-05-11 10:25:17 +0800

    feat(daily): 04.29

---
+++ daily_pracice/2022.04/0429.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func slowFunc2(c chan string) {
+	time.Sleep(time.Second * 2)
+	c <- "slowFunc() Finished"
+}
+
+func main() {
+	c := make(chan string)
+	go slowFunc2(c)
+
+	msg := <-c
+	fmt.Println(msg)
+}
